2cda76681326ae330417190bee97824dd5446b12
Andrew James <contact@ajames.dev>
2020-02-11 18:44:05 +0000

    goooo

--- src/containers/Home/index.tsx
+++ src/containers/Home/index.tsx
@@ -2,25 +2,22 @@ import { RouteComponentProps } from '@reach/router';
 
 import React from 'react';
 
-export const Home = (props: RouteComponentProps) => {
-  const foo = 'bang';
-  let bar = 1;
-  return (
-    <div {...props}>
-      <header className="App-header">
-        <p className="y">
-          Edit lsc
-          <code> src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-};
+export const Home = (props: RouteComponentProps) => (
+  <div>
+    <header className="App-header">
+      <p>
+        Edit lsc
+        <code> src/App.tsx</code> and save to reload.
+        <pre>{JSON.stringify(props)}</pre>
+      </p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+    </header>
+  </div>
+);
